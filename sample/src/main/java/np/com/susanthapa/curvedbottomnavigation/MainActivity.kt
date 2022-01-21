5cc269e102b7c84d005c76e95b6ab30938056ea2
ahamedjaber8 <ahamedjaber8@gmail.com>
2022-01-21 19:41:39 +0600

    circle update

---
+++ sample/src/main/java/np/com/susanthapa/curvedbottomnavigation/MainActivity.kt
@@ -0,0 +1,71 @@
+package np.com.susanthapa.curvedbottomnavigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import np.com.susanthapa.curved_bottom_navigation.CbnMenuItem
+import np.com.susanthapa.curvedbottomnavigation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val activeIndex = savedInstanceState?.getInt("activeIndex") ?: 2
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_dashboard,
+                R.id.navigation_notifications,
+                R.id.navigation_profile,
+                R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        val menuItems = arrayOf(
+            CbnMenuItem(
+                R.drawable.ic_notification,
+                R.drawable.ic_launcher_background,
+                R.id.navigation_notifications
+            ),
+            CbnMenuItem(
+                R.drawable.ic_dashboard,
+                R.drawable.ic_launcher_background,
+                R.id.navigation_dashboard
+            ),
+            CbnMenuItem(
+                R.drawable.ic_home,
+                R.drawable.ic_launcher_background,
+                R.id.navigation_home
+            ),
+            CbnMenuItem(
+                R.drawable.ic_profile,
+                R.drawable.ic_launcher_background,
+                R.id.navigation_profile
+            ),
+            CbnMenuItem(
+                R.drawable.ic_settings,
+                R.drawable.ic_launcher_background,
+                R.id.navigation_settings
+            )
+        )
+
+        binding.navView.setMenuItems(menuItems, activeIndex)
+        binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putInt("activeIndex", binding.navView.getSelectedIndex())
+        super.onSaveInstanceState(outState)
+    }
+}
